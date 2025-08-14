2b345621143727b5266fd6520fe3b8c611beb238
Daniel Britto <danielbritto185@gmail.com>
2025-08-14 16:37:44 -0500

    Initial Commit

---
+++ Changelle/ChangelleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangelleApp.swift
+//  Changelle
+//
+//  Created by Daniel Stiven Britto Argote on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ChangelleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Changelle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Changelle
+//
+//  Created by Daniel Stiven Britto Argote on 14/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
